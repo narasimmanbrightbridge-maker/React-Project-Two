7b34a2a99d9b9d6ea123644239ddf59e40a83776
narasimman <narasimmanbrightbridge@gmail.com>
2026-03-21 17:23:49 +0530

    day-11-02

--- src/components/Home/main-banner.tsx
+++ src/components/Home/main-banner.tsx
@@ -3,7 +3,7 @@ function MainBanner () {
         <section className="MainBanner mt-24 mb-24">
             <div className="">
                 <h2 className="font-bold pl-10 pr-10 mb-5 text-3xl">Super Charge Your Workflow</h2>
-                <p className="pr-32 pl-32">Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.Donec velit neque auctor sit amet aliquam vel ullamcorper sit amet ligula.</p>
+                <p className="block m-auto md:pl-32">Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.Donec velit neque auctor sit amet aliquam vel ullamcorper sit amet ligula.</p>
                 <button>Explore Now</button>
             </div>
         </section>
